337a4580bca5919f969c717418b9d4ac63bf983b
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2023-11-11 00:06:07 +0200

    Create App.js

---
+++ React/Intro to React/PersonCard/App.js
@@ -0,0 +1,15 @@
+import PersonCard from './components/PersonCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName={ "Doe" } lastName={ "Jane" } age={45} hairColor = {'Black'} />
+      <PersonCard firstName={ "Smith" } lastName={ "John" } age={88} hairColor = {'Brown'} />
+      <PersonCard firstName={ "Fillmore" } lastName={ "Millard" } age={50} hairColor = {'Brown'} />
+      <PersonCard firstName={ "Smith" } lastName={ "Maria" } age={62} hairColor = {'Brown'} />
+    </div>
+  );
+}
+
+export default App;
