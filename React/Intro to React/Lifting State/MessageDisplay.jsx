92b0bab0751ba13a9e68afb2a7ea73ba9f3fb80c
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2023-11-18 20:05:22 +0200

    Create MessageDisplay.jsx

---
+++ React/Intro to React/Lifting State/MessageDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+    
+const MessageDisplay = (props) => {
+    return (
+        <>
+            <h1>Current Message</h1>
+            <pre>{ props.message }</pre>
+        </>
+    );
+};    
+export default MessageDisplay;
