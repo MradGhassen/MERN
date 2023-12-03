2eb1951e07a6f875a7b8ec23470c2b3f26fd1ef8
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2023-12-03 17:15:59 +0200

    Create joke.model.js

---
+++ MongoDB/jokes/server/models/joke.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const JokeSchema = new mongoose.Schema({
+    setup: {
+        type: String
+    },
+    punchline: {
+        type: Number
+    }
+});
+const Joke = mongoose.model('Joke', JokeSchema);
+module.exports = Joke;
