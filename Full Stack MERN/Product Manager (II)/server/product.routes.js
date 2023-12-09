8ce5cc482bd1c14c3612693c15ff3ff579acbf66
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2023-12-09 11:17:48 +0200

    Create product.routes.js

---
+++ Full Stack MERN/Product Manager (II)/server/product.routes.js
@@ -0,0 +1,7 @@
+const ProductController = require('../controllers/product.controller');
+module.exports = (app) => {
+    app.get('/api', ProductController.index);
+    app.get('/api/product', ProductController.getAllProduct); 
+    app.post('/api/product', ProductController.createProduct);   
+    app.get('/api/product/:id', ProductController.getProduct);
+}
