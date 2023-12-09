96eba1657992d6fbe2df483a718a2080d8d2f597
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2023-12-09 11:18:27 +0200

    Create product.controller.js

---
+++ Full Stack MERN/Product Manager (II)/server/product.controller.js
@@ -0,0 +1,29 @@
+const Product = require('../models/product.model');   
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+module.exports.createProduct = (request, response) => {
+    // Mongoose's "create" method is run using our Product model to add a new product to our db's product collection.
+    // request.body will contain something like {Title: " ", Price: " ", Descripssion: ''} from the client
+    Product.create(request.body) //This will use whatever the body of the client's request sends over
+        .then(person => response.json(person))
+        .catch(err => response.json(err));
+}
+module.exports.getAllProduct = (request, response) => {
+    Product.find({})
+        .then(persons => {
+            console.log(persons); //console logs are optional, but they are highly recommended for troubleshooting!
+            response.json(persons);
+        })
+        .catch(err => {
+            console.log(err)
+            response.json(err)
+        })
+}
+module.exports.getProduct = (request, response) => {
+    Product.findOne({_id:request.params.id})
+        .then(person => response.json(person))
+        .catch(err => response.json(err));
+}
