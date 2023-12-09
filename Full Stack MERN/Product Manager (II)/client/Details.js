aaa83f7b609d931e2cc21ae915cc652c5bcf7aea
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2023-12-09 11:13:30 +0200

    Create Details.js

---
+++ Full Stack MERN/Product Manager (II)/client/Details.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import {useParams} from "react-router-dom";
+const Detail = (props) => {
+    const [product, setProduct] = useState({})
+    const {id} = useParams(); 
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/product/" + id)
+            .then( res => {
+                console.log(res.data);
+                setProduct(res.data);
+            })
+            .catch( err => console.log(err) );
+    }, []);
+    return (
+        <div>
+            <p>Title: {product.Title}</p>
+            <p>Price: {product.Price}</p>
+            <p>Descripssion: {product.Descripssion}</p>
+        </div>
+    );
+}
+export default Detail;
